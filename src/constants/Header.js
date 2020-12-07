dffabff5f0449c0dae0ca5f8d56c8a999c70c620
Tainah Boarini Bernardo <tainahboarini@gmail.com>
2020-12-07 17:01:55 -0300

    Estilização da tela do Profile. Falta o ícone di lápis

---
+++ src/constants/Header.js
@@ -0,0 +1,43 @@
+import styled from "styled-components"
+import React from 'react';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
+  const Bar = styled.div`
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  width: 100%;
+  height: 64px;
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
+  box-shadow: 0 0.5px 0 0 gray;
+  background-color: #ffffff;  
+`;
+  const Title = styled.div`
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  width: 175px;
+  height: 44px;
+  margin: 20px 92px 0 93px;
+  padding: 13px 42px 12px;
+  width: 91px;
+  height: 19px;
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  color: #000000;  
+`;
+
+export default function Header() {
+  return (    
+        <Bar>
+            <Title>Header</Title>
+        </Bar>   
+  );
+}
--- src/routes/coordinator.js
+++ src/routes/coordinator.js
@@ -1,15 +1,12 @@
 export const goToLogin = (history) => {
   history.push('/login')
 }
-
 export const goToSignUp = (history) => {
   history.push('/signup')
 }
-
 export const goToHome = (history) => {
-  history.push('/')
+  history.push('/home')
 }
-
 export const goToRestaurantsDetails = (history, id) => {
   history.push(`/restaurants/${id}`)
 }
